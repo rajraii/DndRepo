e446b8920fa1980e0de66fb82f11ae43a2e54609
Raj Rai <rrai1590@gmail.com>
2022-08-17 13:42:58 +0530

    error handling + env addedd

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [result, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const fetchData = async () => {
     setLoading(true);
-    const clientId = "XyKWvUzHmOEhx2KtkcXVpVRMPWgeEEDlfqW7OJ2RtNE";
-    const url = `https://api.unsplash.com/photos/random?client_id=${clientId}&count=5`;
+    // console.log(process.env.REACT_APP_IMAGE_URL)
+    const url = `${process.env.REACT_APP_URL}random?client_id=${process.env.REACT_APP_CLIENT_ID}&count=5`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setResults(data);
-      });
+      }).catch(error => console.log(error));
     setLoading(false);
   }
   useEffect(() => {
@@ -23,11 +24,14 @@ function App() {
   }, []);
   return (
     <div className="App">
-      {
+      { error? (<>
+        <h1>Something went wrong</h1>
+      </>) :(
         data.map((item, idx) => (
           <Card item={item} image ={result[idx]} loading={loading} />
-        ))
+        )))
       }
+
     </div>
   );
 }
